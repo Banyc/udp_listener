915e530077291b4e11ee2fd277d36c0297484800
Banyc <36535895+Banyc@users.noreply.github.com>
2024-10-27 00:45:02 +0800

    refactor: renaming; switch obj pool impl

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,13 @@ use std::{
 };
 
 use bytes::BytesMut;
-use lockfree_object_pool::{LinearObjectPool, LinearOwnedReusable};
+use primitive::obj_pool::{ArcObjectPool, ObjectScoped};
 use tokio::net::UdpSocket;
 
 pub const BUFFER_LENGTH: usize = 2_usize.pow(16);
+const OBJ_POOL_SHARDS: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(4) };
 
-pub type Packet = LinearOwnedReusable<BytesMut>;
+pub type Packet = ObjectScoped<BytesMut>;
 
 pub type Dispatch<K, V> = Arc<dyn Fn(SocketAddr, Packet) -> Option<(K, V)> + Sync + Send + 'static>;
 
@@ -19,10 +20,10 @@ type ConnTable<K, V> = Arc<Mutex<HashMap<K, tokio::sync::mpsc::Sender<V>>>>;
 
 /// Manage user-defined sub-connections under a UDP socket.
 pub struct UdpListener<K, V> {
-    connected: bool,
+    is_udp_connected: bool,
     udp: Arc<UdpSocket>,
     conn_table: ConnTable<K, V>,
-    buf_pool: Arc<LinearObjectPool<BytesMut>>,
+    pkt_buf_pool: ArcObjectPool<BytesMut>,
     dispatcher_buffer_size: NonZeroUsize,
     dispatch: Dispatch<K, V>,
 }
@@ -42,17 +43,17 @@ impl<K, V> UdpListener<K, V> {
         dispatcher_buffer_size: NonZeroUsize,
         dispatch: Dispatch<K, V>,
     ) -> Self {
-        let accepted = Arc::new(Mutex::new(HashMap::new()));
-        let buf_pool = Arc::new(LinearObjectPool::new(
+        let pkt_buf_pool = ArcObjectPool::new(
+            usize::try_from(u32::MAX).unwrap(),
+            OBJ_POOL_SHARDS,
             || BytesMut::with_capacity(BUFFER_LENGTH),
             |buf| buf.clear(),
-        ));
-        let connected = udp.peer_addr().is_ok();
+        );
         Self {
-            connected,
+            is_udp_connected: udp.peer_addr().is_ok(),
             udp: Arc::new(udp),
-            conn_table: accepted,
-            buf_pool,
+            conn_table: Arc::new(Mutex::new(HashMap::new())),
+            pkt_buf_pool,
             dispatcher_buffer_size,
             dispatch,
         }
@@ -70,21 +71,21 @@ where
     /// # Cancel safety
     ///
     /// This method is cancel safe.
-    pub async fn accept(&self) -> std::io::Result<AcceptedUdp<K, V>> {
+    pub async fn accept(&self) -> std::io::Result<Conn<K, V>> {
         loop {
-            let mut buf = self.buf_pool.pull_owned();
-            let (n, addr) = if self.connected {
-                let n = self.udp.recv_buf(&mut *buf).await?;
+            let mut pkt_buf = self.pkt_buf_pool.take_scoped();
+            let (n, addr) = if self.is_udp_connected {
+                let n = self.udp.recv_buf(&mut *pkt_buf).await?;
                 let addr = self.udp.peer_addr()?;
                 (n, addr)
             } else {
-                self.udp.recv_buf_from(&mut *buf).await?
+                self.udp.recv_buf_from(&mut *pkt_buf).await?
             };
             if n == BUFFER_LENGTH {
                 continue;
             }
 
-            let Some((key, mut value)) = (self.dispatch)(addr, buf) else {
+            let Some((key, mut value)) = (self.dispatch)(addr, pkt_buf) else {
                 continue;
             };
 
@@ -119,50 +120,50 @@ where
     /// Return [`None`] if either:
     ///
     /// - The UDP socket is unconnected;
-    /// - The `dispatch_key` has already been registered in the connection table.
-    pub fn open(&self, dispatch_key: K) -> Option<AcceptedUdp<K, V>> {
+    /// - The `conn_key` has already been registered in the connection table.
+    pub fn open(&self, conn_key: K) -> Option<Conn<K, V>> {
         let peer_addr = self.udp.peer_addr().ok()?;
         let mut conn_table = self.conn_table.lock().unwrap();
-        if conn_table.get(&dispatch_key).is_some() {
+        if conn_table.get(&conn_key).is_some() {
             return None;
         }
         let (tx, rx) = tokio::sync::mpsc::channel(self.dispatcher_buffer_size.get());
-        conn_table.insert(dispatch_key.clone(), tx);
+        conn_table.insert(conn_key.clone(), tx);
         drop(conn_table);
-        Some(self.wrap_handle(dispatch_key, rx, peer_addr))
+        Some(self.wrap_handle(conn_key, rx, peer_addr))
     }
 
     /// Pass in `peer_addr` as [`None`] iff the underlying UDP socket is connected.
     fn wrap_handle(
         &self,
-        dispatch_key: K,
+        conn_key: K,
         rx: tokio::sync::mpsc::Receiver<V>,
         peer_addr: SocketAddr,
-    ) -> AcceptedUdp<K, V> {
-        let close_token = AcceptedUdpCloseToken {
-            dispatch_key: dispatch_key.clone(),
-            accepted: self.conn_table.clone(),
+    ) -> Conn<K, V> {
+        let close_token = ConnCloseToken {
+            conn_key: conn_key.clone(),
+            conn_table: self.conn_table.clone(),
         };
         let close_token = Arc::new(close_token);
-        let read = AcceptedUdpRead {
+        let read = ConnRead {
             recv: rx,
             _close_token: close_token.clone(),
         };
-        let udp_to = if self.connected {
+        let udp_to = if self.is_udp_connected {
             assert_eq!(peer_addr, self.udp.peer_addr().unwrap());
             None
         } else {
             Some(peer_addr)
         };
-        let write = AcceptedUdpWrite {
+        let write = ConnWrite {
             udp: Arc::clone(&self.udp),
             peer: udp_to,
             _close_token: close_token,
         };
-        AcceptedUdp {
+        Conn {
             read,
             write,
-            dispatch_key,
+            conn_key,
         }
     }
 }
@@ -180,96 +181,96 @@ where
 }
 
 trait StaticDrop: Sync + Send + 'static {}
-impl<K, V> StaticDrop for AcceptedUdpCloseToken<K, V>
+impl<K, V> StaticDrop for ConnCloseToken<K, V>
 where
     K: Clone + core::hash::Hash + Eq + Sync + Send + 'static,
     V: Sync + Send + 'static,
 {
 }
 
-struct AcceptedUdpCloseToken<K, V>
+struct ConnCloseToken<K, V>
 where
     K: Clone + core::hash::Hash + Eq,
 {
-    dispatch_key: K,
-    accepted: ConnTable<K, V>,
+    conn_key: K,
+    conn_table: ConnTable<K, V>,
 }
-impl<K, V> Drop for AcceptedUdpCloseToken<K, V>
+impl<K, V> Drop for ConnCloseToken<K, V>
 where
     K: Clone + core::hash::Hash + Eq,
 {
     fn drop(&mut self) {
-        let mut accepted = self.accepted.lock().unwrap();
-        accepted.remove(&self.dispatch_key);
+        let mut conn_table = self.conn_table.lock().unwrap();
+        conn_table.remove(&self.conn_key);
     }
 }
-impl<K, V> core::fmt::Debug for AcceptedUdpCloseToken<K, V>
+impl<K, V> core::fmt::Debug for ConnCloseToken<K, V>
 where
     K: core::fmt::Debug + Clone + core::hash::Hash + Eq,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("AcceptedUdpCloseToken")
-            .field("dispatch_key", &self.dispatch_key)
+            .field("dispatch_key", &self.conn_key)
             .finish()
     }
 }
 
-pub struct AcceptedUdp<K, V> {
-    read: AcceptedUdpRead<V>,
-    write: AcceptedUdpWrite,
-    dispatch_key: K,
+pub struct Conn<K, V> {
+    read: ConnRead<V>,
+    write: ConnWrite,
+    conn_key: K,
 }
-impl<K, V> AcceptedUdp<K, V> {
-    pub fn read(&mut self) -> &mut AcceptedUdpRead<V> {
+impl<K, V> Conn<K, V> {
+    pub fn read(&mut self) -> &mut ConnRead<V> {
         &mut self.read
     }
 
-    pub fn write(&self) -> &AcceptedUdpWrite {
+    pub fn write(&self) -> &ConnWrite {
         &self.write
     }
 
-    pub fn dispatch_key(&self) -> &K {
-        &self.dispatch_key
+    pub fn conn_key(&self) -> &K {
+        &self.conn_key
     }
 
-    pub fn split(self) -> (AcceptedUdpRead<V>, AcceptedUdpWrite) {
+    pub fn split(self) -> (ConnRead<V>, ConnWrite) {
         (self.read, self.write)
     }
 }
-impl<K: core::fmt::Debug, V> core::fmt::Debug for AcceptedUdp<K, V> {
+impl<K: core::fmt::Debug, V> core::fmt::Debug for Conn<K, V> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("AcceptedUdp")
+        f.debug_struct("Conn")
             .field("read", &self.read)
             .field("write", &self.write)
-            .field("dispatch_key", &self.dispatch_key)
+            .field("conn_key", &self.conn_key)
             .finish()
     }
 }
 
-pub struct AcceptedUdpRead<V> {
+pub struct ConnRead<V> {
     recv: tokio::sync::mpsc::Receiver<V>,
     _close_token: Arc<dyn StaticDrop>,
 }
-impl<V> AcceptedUdpRead<V> {
+impl<V> ConnRead<V> {
     pub fn recv(&mut self) -> &mut tokio::sync::mpsc::Receiver<V> {
         &mut self.recv
     }
 }
-impl<V> core::fmt::Debug for AcceptedUdpRead<V> {
+impl<V> core::fmt::Debug for ConnRead<V> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("AcceptedUdpRead")
+        f.debug_struct("ConnRead")
             .field("recv.len()", &self.recv.len())
             .finish()
     }
 }
 
 #[derive(Clone)]
-pub struct AcceptedUdpWrite {
+pub struct ConnWrite {
     udp: Arc<UdpSocket>,
     peer: Option<SocketAddr>,
     _close_token: Arc<dyn StaticDrop>,
 }
-impl AcceptedUdpWrite {
+impl ConnWrite {
     pub fn local_addr(&self) -> std::io::Result<SocketAddr> {
         self.udp.local_addr()
     }
@@ -295,9 +296,9 @@ impl AcceptedUdpWrite {
         }
     }
 }
-impl core::fmt::Debug for AcceptedUdpWrite {
+impl core::fmt::Debug for ConnWrite {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("AcceptedUdpWrite")
+        f.debug_struct("ConnWrite")
             .field("udp", &self.udp)
             .field("peer", &self.peer)
             .finish()
@@ -394,7 +395,7 @@ mod tests {
                     }
                 }
             });
-            assert_eq!(*conn.dispatch_key(), key);
+            assert_eq!(*conn.conn_key(), key);
             let packet = conn.read().recv().recv().await.unwrap();
             assert_eq!(packet.as_ref(), msg);
             let buf = [key].iter().chain(msg).copied().collect::<Vec<u8>>();
@@ -411,7 +412,7 @@ mod tests {
                     }
                 }
             });
-            assert_eq!(*conn.dispatch_key(), key);
+            assert_eq!(*conn.conn_key(), key);
             let buf = [key].iter().chain(msg).copied().collect::<Vec<u8>>();
             conn.write().send(&buf).await.unwrap();
             let packet = conn.read().recv().recv().await.unwrap();
