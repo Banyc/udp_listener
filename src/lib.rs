8394e398dfbc575c2811dab22bd535b807e8e1e6
Banyc <36535895+Banyc@users.noreply.github.com>
2026-07-22 23:42:43 +0800

    udp_listener: add UnreliableTransmit impl for tokio::net::UdpSocket and tokio_udp::UdpSocket; move tests to tokio_udp

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,9 @@ use std::{
 
 use bytes::{BufMut, BytesMut};
 use primitive::arena::obj_pool::{ArcObjPool, ObjScoped};
-use tokio::net::UdpSocket;
 
 pub const PACKET_BUFFER_LENGTH: usize = 2_usize.pow(16);
-const OBJ_POOL_SHARDS: NonZeroUsize = unsafe { NonZeroUsize::new_unchecked(4) };
+const OBJ_POOL_SHARDS: NonZeroUsize = NonZeroUsize::new(4).unwrap();
 
 pub type Packet = ObjScoped<BytesMut>;
 
@@ -341,6 +340,43 @@ where
     }
 }
 
+async fn default_send_vectored<U: UnreliableTransmit>(
+    this: &U,
+    bufs: &[IoSlice<'_>],
+) -> std::io::Result<usize> {
+    match bufs.len() {
+        0 => Ok(0),
+        1 => this.send(&bufs[0]).await,
+        _ => {
+            let total = bufs.iter().map(|b| b.len()).sum();
+            let mut buf = Vec::with_capacity(total);
+            for b in bufs {
+                buf.extend_from_slice(b);
+            }
+            this.send(&buf).await
+        }
+    }
+}
+
+async fn default_send_to_vectored<U: UnreliableTransmit>(
+    this: &U,
+    bufs: &[IoSlice<'_>],
+    target: &U::ProtocolAddress,
+) -> std::io::Result<usize> {
+    match bufs.len() {
+        0 => Ok(0),
+        1 => this.send_to(&bufs[0], target).await,
+        _ => {
+            let total = bufs.iter().map(|b| b.len()).sum();
+            let mut buf = Vec::with_capacity(total);
+            for b in bufs {
+                buf.extend_from_slice(b);
+            }
+            this.send_to(&buf, target).await
+        }
+    }
+}
+
 pub trait UnreliableTransmit {
     type ProtocolAddress: Clone;
     fn local_addr(&self) -> std::io::Result<Self::ProtocolAddress>;
@@ -356,49 +392,17 @@ pub trait UnreliableTransmit {
         buf: &[u8],
         target: &Self::ProtocolAddress,
     ) -> impl Future<Output = std::io::Result<usize>>;
-    fn send_vectored(
-        &self,
-        bufs: &[IoSlice<'_>],
-    ) -> impl Future<Output = std::io::Result<usize>> {
-        async move {
-            match bufs.len() {
-                0 => Ok(0),
-                1 => self.send(&*bufs[0]).await,
-                _ => {
-                    let total = bufs.iter().map(|b| b.len()).sum();
-                    let mut buf = Vec::with_capacity(total);
-                    for b in bufs {
-                        buf.extend_from_slice(b);
-                    }
-                    self.send(&buf).await
-                }
-            }
-        }
-    }
+    fn send_vectored(&self, bufs: &[IoSlice<'_>]) -> impl Future<Output = std::io::Result<usize>>;
     fn send_to_vectored(
         &self,
         bufs: &[IoSlice<'_>],
         target: &Self::ProtocolAddress,
-    ) -> impl Future<Output = std::io::Result<usize>> {
-        async move {
-            match bufs.len() {
-                0 => Ok(0),
-                1 => self.send_to(&*bufs[0], target).await,
-                _ => {
-                    let total = bufs.iter().map(|b| b.len()).sum();
-                    let mut buf = Vec::with_capacity(total);
-                    for b in bufs {
-                        buf.extend_from_slice(b);
-                    }
-                    self.send_to(&buf, target).await
-                }
-            }
-        }
-    }
+    ) -> impl Future<Output = std::io::Result<usize>>;
     fn try_send(&self, buf: &[u8]) -> std::io::Result<usize>;
     fn try_send_to(&self, buf: &[u8], target: &Self::ProtocolAddress) -> std::io::Result<usize>;
+    fn is_send_vectored(&self) -> bool;
 }
-impl UnreliableTransmit for UdpSocket {
+impl UnreliableTransmit for tokio::net::UdpSocket {
     type ProtocolAddress = SocketAddr;
     fn local_addr(&self) -> std::io::Result<SocketAddr> {
         self.local_addr()
@@ -427,6 +431,63 @@ impl UnreliableTransmit for UdpSocket {
     fn try_send_to(&self, buf: &[u8], target: &Self::ProtocolAddress) -> std::io::Result<usize> {
         self.try_send_to(buf, *target)
     }
+    async fn send_vectored(&self, bufs: &[IoSlice<'_>]) -> std::io::Result<usize> {
+        default_send_vectored(self, bufs).await
+    }
+    async fn send_to_vectored(
+        &self,
+        bufs: &[IoSlice<'_>],
+        target: &Self::ProtocolAddress,
+    ) -> std::io::Result<usize> {
+        default_send_to_vectored(self, bufs, target).await
+    }
+    fn is_send_vectored(&self) -> bool {
+        false
+    }
+}
+
+impl UnreliableTransmit for tokio_udp::UdpSocket {
+    type ProtocolAddress = SocketAddr;
+    fn local_addr(&self) -> std::io::Result<SocketAddr> {
+        self.local_addr()
+    }
+    fn peer_addr(&self) -> std::io::Result<SocketAddr> {
+        self.peer_addr()
+    }
+    async fn recv_buf(&self, buf: &mut impl BufMut) -> std::io::Result<usize> {
+        self.recv_buf(buf).await
+    }
+    async fn recv_buf_from(
+        &self,
+        buf: &mut impl BufMut,
+    ) -> std::io::Result<(usize, Self::ProtocolAddress)> {
+        self.recv_buf_from(buf).await
+    }
+    async fn send(&self, buf: &[u8]) -> std::io::Result<usize> {
+        self.send(buf).await
+    }
+    async fn send_to(&self, buf: &[u8], target: &Self::ProtocolAddress) -> std::io::Result<usize> {
+        self.send_to_vectored(&[IoSlice::new(buf)], target).await
+    }
+    fn try_send(&self, buf: &[u8]) -> std::io::Result<usize> {
+        self.try_send(buf)
+    }
+    fn try_send_to(&self, buf: &[u8], target: &Self::ProtocolAddress) -> std::io::Result<usize> {
+        self.try_send_to(buf, target)
+    }
+    async fn send_vectored(&self, bufs: &[IoSlice<'_>]) -> std::io::Result<usize> {
+        self.send_vectored(bufs).await
+    }
+    async fn send_to_vectored(
+        &self,
+        bufs: &[IoSlice<'_>],
+        target: &Self::ProtocolAddress,
+    ) -> std::io::Result<usize> {
+        self.send_to_vectored(bufs, target).await
+    }
+    fn is_send_vectored(&self) -> bool {
+        tokio_udp::is_vectored_supported()
+    }
 }
 
 #[cfg(test)]
@@ -441,7 +502,9 @@ mod tests {
     #[tokio::test(flavor = "multi_thread")]
     async fn test_listener() {
         let dispatcher_buffer_size = NonZeroUsize::new(2).unwrap();
-        let udp = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+        let udp = tokio_udp::UdpSocket::bind("127.0.0.1:0".parse().unwrap())
+            .await
+            .unwrap();
         let listen_addr = udp.local_addr().unwrap();
         let listener = UtpListener::new_identity_dispatch(udp, dispatcher_buffer_size);
         let send_msg_1 = b"hello";
@@ -468,8 +531,10 @@ mod tests {
         let client_recv_msg = client_recv_msg.notified();
         let second_accept = second_accept.notified();
 
-        let client = UdpSocket::bind("127.0.0.1:0").await.unwrap();
-        client.connect(listen_addr).await.unwrap();
+        let client = tokio_udp::UdpSocket::bind("127.0.0.1:0".parse().unwrap())
+            .await
+            .unwrap();
+        client.connect(listen_addr).unwrap();
         client.send(send_msg_1).await.unwrap();
         client.send(send_msg_2).await.unwrap();
         client_recv_msg.await;
@@ -497,14 +562,18 @@ mod tests {
         };
         let dispatch = Arc::new(dispatch);
 
-        let server = UdpSocket::bind("127.0.0.1:0").await.unwrap();
+        let server = tokio_udp::UdpSocket::bind("127.0.0.1:0".parse().unwrap())
+            .await
+            .unwrap();
         let listen_addr = server.local_addr().unwrap();
-        let server: UtpListener<UdpSocket, u8, Packet> =
+        let server: UtpListener<tokio_udp::UdpSocket, u8, Packet> =
             UtpListener::new(server, dispatcher_buffer_size, dispatch.clone());
 
-        let client = UdpSocket::bind("127.0.0.1:0").await.unwrap();
-        client.connect(listen_addr).await.unwrap();
-        let client: UtpListener<UdpSocket, u8, Packet> =
+        let client = tokio_udp::UdpSocket::bind("127.0.0.1:0".parse().unwrap())
+            .await
+            .unwrap();
+        client.connect(listen_addr).unwrap();
+        let client: UtpListener<tokio_udp::UdpSocket, u8, Packet> =
             UtpListener::new(client, dispatcher_buffer_size, dispatch.clone());
 
         let mut tasks = tokio::task::JoinSet::new();
